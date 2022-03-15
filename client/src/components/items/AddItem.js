de49dae0a46fe82e9e0b3c791959ef1df0c709aa
Abdul Alhumidan <abdulalhumidan@LP1-US-51819350.local>
2022-03-15 13:49:12 -0300

    added page layout for add item page

--- client/src/components/items/AddItem.js
+++ client/src/components/items/AddItem.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
 const AddItem = () => {
+
+    const handleSubmit = (e) => {
+
+    }
     return (
-        <div>AddItem</div>
+        <div>
+            <form className="ui form" onSubmit={handleSubmit}>
+                <div className="field">
+                    <label>Item Name</label>
+                    <input type="text" name="type" placeholder="Item Name" onChange={(e) => {}}/>
+                </div>
+                <div className="field">
+                    <select className="ui dropdown">
+                        <option value="">Gender</option>
+                        <option value="1">Male</option>
+                        <option value="0">Female</option>
+                    </select>
+                </div>
+                <div className="field">
+                    <div className="ui checkbox">
+                        <input type="checkbox" name="tracked" tabIndex="0" onChange={e => {}} />
+                        <label>Track this item</label>
+                    </div>
+                </div>
+                <div className="field">
+                    <label>Low Supply Limit</label>
+                    <input type="text" name="lowSupplyLimit" placeholder="low Supply Limit" onChange={(e) => {}}/>
+                </div>
+                <button className="ui button" type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
 
