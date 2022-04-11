85f27c6d32ef7ace2bba8d4f4a2a67968241b30d
Abdul Alhumidan <abdulalhumidan@LP1-US-51819350.local>
2022-04-11 11:45:22 -0300

    Improved UI for listing items

--- client/src/components/items/Items.js
+++ client/src/components/items/Items.js
@@ -10,6 +10,10 @@ import { updateTypeCount } from '../../actions/types';
 const Items = () => {
   const navigate = useNavigate();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const uniqueTypes = [];
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const items = useSelector((state) => state.items);
 
   const onEditClick = (item) => {
     navigate('/additem', {state:{item: item}});
@@ -23,8 +27,18 @@ const Items = () => {
         dispatch(getItems(user?.result.email));
     }, [dispatch]);
 
+    for(let i = 0 ; i<items?.length ; i++) {
+      if(!uniqueTypes.includes(items[i]?.type.name)) {
+        uniqueTypes.push(items[i]?.type.name);
+      }
+    }
+
     const handleOnChange = (e) => {
       console.log(e.target.value)
+    }
+
+    const onTitleClick = (index) => {
+      setActiveIndex(index);
     }
 
     const handleDelete = (item) => {
@@ -32,10 +46,17 @@ const Items = () => {
       dispatch(deleteItem(item._id))
     }
 
-    const items = useSelector((state) => state.items);
-
     return (
-        <table className="ui celled table">
+        uniqueTypes.map( (uniqueType, index) => {
+          const active = index === activeIndex ? 'active' : '';
+          return (
+            <div className="ui styled fluid accordion" key={uniqueType}>
+            <div className={`title ${active[index] ? 'active' : ''}`} onClick={() => onTitleClick(index)}>
+              <i className="dropdown icon"></i>
+                {uniqueType}
+            </div>
+            <div className={`content ${active[index] ? 'active' : ''}`}>
+            <table className="ui celled table">
           <thead>
             <tr>
               <th>Item Name</th>
@@ -47,12 +68,13 @@ const Items = () => {
           </thead>
           <tbody>
             {items.map((item) => {
+              if(item.type.name === uniqueType)
               return (
                 <tr key={item._id}>
                   <td data-label="name">{item.name}</td>
                   <td data-label="type">{item.type.name}</td>
                   <td data-label="count">{item.count}</td>
-                  <td data-label="tracked">{item.tracked ? 'True' : 'False'}</td>
+                  <td data-label="tracked">{item.tracked ? <i className="large green checkmark icon"></i> : <i></i>}</td>
                   <td data-label="lowsupplycount">{item.lowSupplyLimit}</td>
                   <td data-label="lowsupplycount">
                         <div className="ui two buttons">
@@ -65,6 +87,10 @@ const Items = () => {
             })}
           </tbody>
         </table>
+            </div>
+         </div>
+          )
+        })
     );
 }
 
